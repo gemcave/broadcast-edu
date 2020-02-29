5d5dd23fd131afa4c4e849276e9a3b7ffd10434d
yerowyn <yerowyn@gmail.com>
2020-03-01 01:18:12 +0600

    create broadcast

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ChatContainer from "../containers/ChatContainer";
+import FormContainer from "../containers/FormContainer";
+
+const channel = new BroadcastChannel("main");
 
 const Homepage = props => {
+	const [received, setReceived] = useState([]);
+	const [sent, setSent] = useState([]);
+
+	useEffect(() => {
+		channel.onmessage = ({data}) => {
+			setReceived(prevState => ([...prevState, { ...data, type: "reveived" }]));
+		}
+	}, []);
+
     return (
         <div>
             Time to start coding!
+						<FormContainer 
+							setSent={setSent}
+							channel={channel}
+							/>
+						<ChatContainer 
+							chats={sent.concat(received)}/>
         </div>
     );
 };
